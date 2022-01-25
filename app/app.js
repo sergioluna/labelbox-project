caafccaae44c14e0d9bedfcacb6a056d88e9e2b5
sergioluna <sluna@sergioluna.dev>
2022-01-24 18:46:16 -0800

    Add refactored code as v2

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const db = require('./database');
+const commands = require('./commands');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -45,6 +46,42 @@ app.get('/api/NASA-APOD', async (req, res) => {
     );
 });
 
+app.get('/api/NASAImage/:api_key', (req, res) => {
+    data = await commands.fetchNASAImage(req.params.api_key);
+    if (!data) {
+        res.status(500).json({
+            status: "error",
+            message: "unexpected error"
+        });
+        return;
+    }
+    const onSuccess = (image) => {
+        res.json({
+            status: "success",
+            image: image
+        });
+    }
+    const onFailure = (error) => {
+        if (error.message.includes("UNIQUE constraint failed")) {
+            res.status(409).json({
+                status: "error",
+                message: "image already exists"
+            });
+        } else {
+            res.status(500).json({
+                status: "error",
+                message: "unexpected error"
+            });
+        }
+    }
+
+    image = {
+        title: data.title,
+        url: data.url
+    };
+    commands.saveNASAImage(image, onSuccess, onFailure)
+});
+
 app.post('/api/user', (req, res) => {
     if (!req.body.email) {
         res.status(400).json({status: "error", message: "email field is required in request body"});
@@ -66,6 +103,40 @@ app.post('/api/user', (req, res) => {
     );
 });
 
+
+app.post('/api/user/v2', (req, res) => {
+    if (!req.body.email) {
+        res.status(400).json({
+            status: "error",
+            message: "invalid request"
+        });
+        return;
+    }
+    const handleSuccess = (user) => {
+        res.json({
+            status: "success",
+            user: user
+        });
+    }
+    const handleFailure = (error) => {
+        if (error.message.includes("UNIQUE constraint failed")) {
+            res.status(409).json({
+                status: "error",
+                message: "user already exists"
+            });
+        } else {
+            res.status(500).json({
+                status: "error",
+                message: "unexpected error"
+            });
+        }
+    }
+
+    let user = { email: req.body.email };
+    commands.createUser(user, handleSuccess, handleFailure);
+});
+
+
 app.delete('/api/user/:user_id', (req, res) => {
     if (!req.params.user_id) {
         res.status(400).json({status: "error", message: "user_id param is required in url (/api/users/{user_id})"});
@@ -86,6 +157,41 @@ app.delete('/api/user/:user_id', (req, res) => {
         }
     );
 });
+
+
+app.delete('/api/user/:user_id/v2', (req, res) => {
+    if (!req.params.user_id){
+        res.status(400).json({
+            status: "error",
+            message: "invalid request"
+        });
+        return;
+    }
+    const onSuccess = () => {
+        res.json({
+            status: "success",
+            message: "user deleted"
+        });
+    }
+    const onFailure = (error) => {
+        if (error.message.includes("not found")) {
+            res.status(404).json({
+                status: "error",
+                message: "user not found"
+            });
+        } else {
+            res.status(500).json({
+                status: "error",
+                message: "unexpected error"
+            });
+        }
+    }
+
+    const user_id = req.params.user_id;
+    commands.deleteUser(user_id, onSuccess, onFailure);
+});
+
+
 
 app.post('/api/rating', (req, res) => {
     if (!req.body.user_id || !req.body.image_id || !req.body.value ) {
@@ -115,6 +221,53 @@ app.post('/api/rating', (req, res) => {
     );
 });
 
+app.post('api/rating/v2', (req, res) => {
+    if (
+        !req.body.user_id ||
+        !req.body.image_id ||
+        !req.body.value ||
+        ![1, 2, 3, 4, 5].includes(req.body.value) 
+    ) {
+            res.status(400).json({
+                status: "error",
+                message: "invalid request"
+            });
+            return;
+    }
+    const onSuccess = (rating) => {
+        res.json({
+            status: "success",
+            rating: rating
+        });
+    }
+    const onFailure = (error) => {
+        if (error.message.includes("UNIQUE constraint failed")) {
+            res.status(409).json({
+                status: "error",
+                message: "rating already exists"
+            });
+        } else if (error.message.includes("FOREIGN KEY constraint failed")) {
+            res.status(404).json({
+                status: "error",
+                message: "user or image not found"
+            });
+        } else {
+            res.status(500).json({
+                status: "error",
+                message: "unexpected error"
+            });
+        }
+    }
+
+    let rating = {
+        user_id: req.body.user_id,
+        image_id: req.body.image_id,
+        value: req.body.value
+    };
+    commands.createRating(rating, onSuccess, onFailure);
+});
+
+
 app.put('/api/rating/:rating_id', (req, res) => {
     if (!req.params.rating_id) {
         res.status(400).json({status: "error", message: "rating_id param is required in url (/api/rating/{rating_id})"});
@@ -141,6 +294,46 @@ app.put('/api/rating/:rating_id', (req, res) => {
     );
 });
 
+app.put('/api/rating/:rating_id/v2', (req, res) => {
+    if (
+        !req.params.rating_id ||
+        !req.body.value ||
+        ![1, 2, 3, 4, 5].includes(req.body.value)
+    ) {
+            res.status(400).json({
+                status: "error",
+                message: "invalid request"
+            });
+            return;
+    }
+    const onSuccess = (rating) => {
+        res.json({
+            status: "success",
+            rating: rating
+        });
+    };
+    const onFailure = (error) => {
+        if (error.message.includes("not found")) {
+            res.status(404).json({
+                status: "error",
+                message: "rating not found"
+            });
+        } else {
+            res.status(500).json({
+                status: "error",
+                message: "unexpected error"
+            });
+        }    
+    }
+
+    rating = {
+        id = req.params.rating_id,
+        value = req.body.value
+    }
+    commands.updateRating(rating, onSuccess, onFailure);
+});
+
+
 app.delete('/api/rating/:rating_id', (req, res) => {
     if (!req.params.rating_id) {
         res.status(400).json({status: "error", message: "rating_id param is required in url (/api/rating/{rating_id})"});
@@ -160,6 +353,40 @@ app.delete('/api/rating/:rating_id', (req, res) => {
             }
         }
     );
+});
+
+app.delete('/api/rating/:rating_id/v2', (req, res) => {
+    if (!req.params.rating_id) {
+        res.status(400).json({
+            status: "error",
+            message: "invalid request"
+        });
+        return;
+    }
+    const onSuccess = () => {
+        res.json({
+            status: "success",
+            message: "rating deleted"
+        });
+    }
+    const onFailure = (error) => {
+        if (error.message.includes("not found")) {
+            res.status(404).json({
+                status: "error",
+                message: "rating not found"
+            });
+        } else {
+            res.status(500).json({
+                status: "error",
+                message: "unexpected error"
+            });
+        }   
+    }
+
+    let rating = {
+        id: req.params.rating_id
+    };
+    commands.deleteRating(rating, onSuccess, onFailure);
 });
 
 app.get('/api/user/:user_id/ratings', (req, res) => {
@@ -190,8 +417,40 @@ app.get('/api/user/:user_id/ratings', (req, res) => {
             }
         }
     );
+});
 
-    
+app.get('/api/user/:user_id/ratings/v2', (req, res) => {
+    if (!req.params.user_id) {
+        res.status(400).json({
+            status: "error",
+            message: "invalid request"
+        });
+        return;
+    }
+    const onSuccess = (rows) => {
+        res.json({
+            status: "success",
+            ratings: rows
+        });
+    }
+    const onFailure = (error) => {
+        if (error.message.includes("not found")) {
+            res.status(404).json({
+                status: "error",
+                message: "rating not found"
+            });
+        } else {
+            res.status(500).json({
+                status: "error",
+                message: "unexpected error"
+            });
+        }
+    }
+
+    let user = {
+        id: req.params.user_id
+    };
+    commands.getUserRatings(user, onSuccess, onFailure);
 });
 
 app.listen(port, () => {
